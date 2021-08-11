460197523473add30ea1559d151c3cc8ea15a76a
ArturHrytsiv-huck <artur.hrytsiv.knm.2019@lpnu.ua>
2021-08-11 10:56:53 +0300

    Interface UI

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     public Canvas pointUI;
+
+    private bool inTrigger = false;
     private void Awake()
     {
         pointUI = Instantiate(pointUI, transform.position, pointUI.transform.rotation);
@@ -21,9 +23,21 @@ public class ItemScript : MonoBehaviour
         Vector3 posUI = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
         pointUI.transform.position = posUI;
         pointUI.gameObject.SetActive(true);
+        inTrigger = true;
     }
     private void OnTriggerExit(Collider other)
     {
         pointUI.gameObject.SetActive(false);
+        inTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (inTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("You took item");
+            pointUI.gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 }
